28cd419fc95188dbf4988d1ffb90e93046317b95
Gurjit Singh <g.s@outlook.in>
2019-09-19 20:38:44 +0530

    stuff

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom'
 
 import DropZone from './components/DropZone'
 
-const App = () => (
-  <div className='container'>
-    <div className='row vh-100 d-flex flex-column align-items-center justify-content-center'>
-      <div className='col-md-8 col-sm-10 col-12'>
-        <div className='card bg-primary rounded-lg h-85 shadow-lg'>
-          <div className='card-body d-flex flex-column'>
-            <DropZone />
+const App = () => {
+  React.useEffect(() => {
+    document.title = `Concat and uniqify text files`
+  })
+
+  return (
+    <div className='container'>
+      <div className='row vh-100 d-flex flex-column align-items-center justify-content-center'>
+        <div className='col-md-8 col-sm-10 col-12'>
+          <div className='card bg-primary rounded-lg h-85 shadow-lg'>
+            <div className='card-body d-flex flex-column'>
+              <DropZone />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
