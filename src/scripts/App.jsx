b2bd089d9386b83fcb1230b4f9cc0139778ee992
Gurjit Singh <g.s@outlook.in>
2019-09-19 22:52:36 +0530

    added GH ribbon

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -10,6 +10,14 @@ const App = () => {
 
   return (
     <div className='container'>
+      <a
+        className='github-fork-ribbon'
+        href='https://github.com/gxjit/concat-and-uniqify-src'
+        data-ribbon='Fork me on GitHub'
+        title='Fork me on GitHub'
+      >
+        Fork me on GitHub
+      </a>
       <div className='row vh-100 d-flex flex-column align-items-center justify-content-center'>
         <div className='col-md-8 col-sm-10 col-12'>
           <div className='card bg-primary rounded-lg h-85 shadow-lg'>
